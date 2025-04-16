98fbf71747515ea6ccd94f288d9d2ea6e8465a1e
Chris <chris79gp@outlook.com>
2025-04-16 22:05:56 +0100

    Feat: add `Node` class

--- src/Node.js
+++ src/Node.js
@@ -1,3 +1,23 @@
-class Node {}
+class Node {
+	constructor(entry = null) {
+		this.entry = entry;
+		this.left = null;
+		this.right = null;
+	}
+
+	setLeft(node) {
+		if (!(node instanceof Node || node === null)) {
+			throw new Error("'left' must be a Node or null");
+		}
+		this.left = node;
+	}
+
+	setRight(node) {
+		if (!(node instanceof Node || node === null)) {
+			throw new Error("'right' must be a Node or null");
+		}
+		this.right = node;
+	}
+}
 
 export default Node;
