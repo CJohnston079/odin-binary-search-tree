c0b30b9898830d11ab4edad41c83e1b0143ababe
Chris <chris79gp@outlook.com>
2025-04-18 21:31:12 +0100

    Build: add structure of `Tree` class

--- src/Tree.js
+++ src/Tree.js
@@ -1,5 +1,57 @@
 import Node from "./Node.js";
 
-class Tree {}
+class Tree {
+	constructor(array = []) {
+		this._root = this.buildTree(array);
+	}
+
+	buildTree(arr) {
+		return [];
+	}
+
+	insert(value) {
+		return;
+	}
+
+	delete(vaue) {
+		return;
+	}
+
+	find(value) {
+		return;
+	}
+
+	levelOrder(cb) {
+		return;
+	}
+
+	inOrder(cb) {
+		return;
+	}
+
+	preOrder(cb) {
+		return;
+	}
+
+	postOrder(cb) {
+		return;
+	}
+
+	height(value) {
+		return;
+	}
+
+	depth(value) {
+		return;
+	}
+
+	isBalanced() {
+		return;
+	}
+
+	rebalance() {
+		return;
+	}
+}
 
 export default Tree;
